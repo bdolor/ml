fe0b18ebcbff56545ff2aca9be15fe0990203128
bdolor <bpayne@bccampus.ca>
2018-10-06 16:28:58 -0700

    add reporting

--- pipeline.php
+++ pipeline.php
@@ -16,6 +16,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Phpml\Classification\SVC;
+use Phpml\Classification\NaiveBayes;
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\Metric\ClassificationReport;
 use Phpml\Pipeline;
 use Phpml\Preprocessing\Imputer;
 use Phpml\Preprocessing\Imputer\Strategy\MedianStrategy;
@@ -48,21 +51,16 @@ $transformers = [
 	new TfIdfTransformer(),
 ];
 $estimator    = new SVC();
+//$estimator = new \Phpml\Classification\NaiveBayes();
+//$estimator = new \Phpml\Classification\KNearestNeighbors();
 
 $targets = [
-	1,
-	1,
-	2,
-	2,
-	3,
-	3,
-];
-
-$map = [
-	1 => 'Biology',
-	2 => 'Arts',
-	3 => 'Trades',
-
+	'Biology',
+	'Biology',
+	'Arts',
+	'Arts',
+	'Trades',
+	'Trades',
 ];
 
 $training_pipeline = new Pipeline( $transformers, $estimator );
@@ -77,13 +75,38 @@ $training_pipeline->train( $training_samples, $targets );
 |
 |
 */
-$new_samples = [
+$new_samples  = [
 	'Knowing Home: Braiding Indigenous Science with Western Science, Book 1 Sciences Biology Since Indigenous peoples have developed time-proven approaches to sustaining both community and environment, Elders and young people are concerned that this rich legacy of Indigenous Science with its wealth of environmental knowledge and the wisdom of previous generations could disappear if it is not respected, studied and understood by today\'s children and youth. A perspective where relationships between home place and all other beings that inhabit the earth is vitally important to all residents—both inheritors of ancient Indigenous Knowledge and wisdom, and newcomers who can experience the engagement, joy and promise of science instilled with a sense of place. This book takes a step forward toward preserving and actively using the knowledge, stories, and lessons for today and future generations, and with it a worldview that informs everyday attitudes toward the earth. Knowing Home: Braiding Indigenous Science with Western Science is far more than a set of research papers or curriculum studies. The project outputs include both, but they are incorporated into a theoretical structure that can provide the methodological basis for future efforts that attempt to develop culturally responsive Indigenous Science curricula in home places. It is not just one or two angels to organize, but multiple interwoven approaches and cases that give this project its exceptional importance. Thus, the project outputs have been organized into two books. Book 1 provides an overview of why traditional knowledge and wisdom should be included in the science curriculum, a window into the science and technologies of the Indigenous peoples who live in Northwestern North America, Indigenous worldview, culturally responsive teaching strategies and curriculum models, and evaluative techniques. It is intended that the rich examples and cases, combined with the resources listed in the appendices, will enable teachers and students to explore Indigenous Science examples in the classroom; and in addition, support the development of culturally appropriate curriculum projects.',
 	'Understanding Media and Culture Arts Art Media and Design This book’s title tells its intent. It is written to help you understand media and culture. The media and culture are so much a part of our days that sometimes it is difficult to step back and appreciate and apprehend their great impact on our lives. The book’s title, and the book itself, begin with a focus squarely on media. Think of your typical day. If you are like many people, you wake to a digital alarm clock or perhaps your cell phone. Soon after waking, you likely have a routine that involves some media. Some people immediately check the cell phone for text messages. Others will turn on the computer and check Facebook, email, or websites. Some people read the newspaper. Others listen to music on an iPod or CD. Some people will turn on the television and watch a weather channel, cable news, or Sports Center. Heading to work or class, you may chat on a cell phone or listen to music. Your classes likely employ various types of media from course management software to PowerPoint presentations to DVDs to YouTube. You may return home and relax with video games, television, movies, more Facebook, or music. You connect with friends on campus and beyond with text messages or Facebook. And your day may end as you fall asleep to digital music. Media for most of us are entwined with almost every aspect of life and work. Understanding media will not only help you appreciate the role of media in your life but also help you be a more informed citizen, a more savvy consumer, and a more successful worker. Media influence all those aspects of life as well.',
 	'Line D: Organizational Skills Competency D-3: Read Drawings and Specifications Trades Common Core Some of the most important documents used in the workplace are the technical drawings, diagrams, and schematics that specify how fabrication and construction tasks will be carried out, or describe the composition and assembly of equipment. One of the essential skills for anyone involved in a trade is the ability to correctly interpret drawings. If you are in a construction or fabrication industry, you will need to be able to examine a drawing, take information from it, and visualize the finished product. If you are in a service or maintenance industry, you will need to interpret exploded drawings in order to properly repair or assemble equipment. The following list of lines and competencies was generated with the goal of creating an entry-level trades training resource, while still offering the flexibility for lines to be used as stand-alone books. Line D – Organizational Skills. D-1 Solve Trades Mathematical Problems, D-2 Apply Science Concepts to Trades Applications, D-3 Read Drawings and Specifications, D-4 Use Codes, Regulations, and Standards, D-5 Use Manufacturer and Supplier Documentation, D-6 Plan Projects. Special thanks to CAPER-BC (https://caperbc.ca/) for creating the audio recording for each Competency.',
 ];
+$actualLabels = [ 'Biology', 'Arts', 'Trades' ];
 
-$predicted[] = $training_pipeline->predict( $new_samples );
+$predicted = $training_pipeline->predict( $new_samples );
 
-print_r( $predicted );
+/*
+|--------------------------------------------------------------------------
+| Reporting
+|--------------------------------------------------------------------------
+|
+|
+|
+|
+*/
+$report = new ClassificationReport( $actualLabels, $predicted );
+echo "<pre>";
 
+echo "<h2>Precision</h2>";
+print_r( $report->getPrecision() );
+
+echo "<h2>Recall</h2>";
+print_r( $report->getRecall() );
+
+echo "<h2>F1 Score</h2>";
+print_r( $report->getF1score() );
+
+echo "<h2>Support</h2>";
+print_r( $report->getSupport() );
+
+echo "<h2>Average</h2>";
+print_r( $report->getAverage() );
